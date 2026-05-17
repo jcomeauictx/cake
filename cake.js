0994f9dc204e48cff418981ec1df07131fec232b
John Comeau <jc@unternet.net>
2026-05-16 17:37:22 -0700

    added onclick, doesn't do anything yet

--- cake.js
+++ cake.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+  const startbutton = document.getElementById("start-button");
+  startbutton.onclick = function(event) {
+    console.log('expanding startbutton');
+    return false;  /* disable bubbling */
+  };
   console.log("cake javascript loaded");
 }
 /* vim: set tabstop=8 softtabstop=2 expandtab shiftwidth=2: */
