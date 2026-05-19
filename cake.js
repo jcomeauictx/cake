2cace1b390039b02f8e10686becb25dca9e69a10
John Comeau <jc@unternet.net>
2026-05-19 08:53:55 -0700

    added maxTouch

--- cake.js
+++ cake.js
@@ -1,9 +1,10 @@
 window.onload = function() {
   const startbutton = document.getElementById("start-button");
+  const maxTouch = navigator.maxTouchPoints ?? navigator.msMaxTouchPoints ?? 0;
   startbutton.onclick = function(event) {
     console.log('expanding startbutton');
     return false;  /* disable bubbling */
   };
-  console.log("cake javascript loaded");
+  console.log("cake javascript loaded: maxTouch = " + maxTouch);
 }
 /* vim: set tabstop=8 softtabstop=2 expandtab shiftwidth=2: */
